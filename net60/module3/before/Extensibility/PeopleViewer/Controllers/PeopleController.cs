935be789fd213a54e209525bca1c564a5d5027a4
Filip Johnsson <Filip.johnsson1989@gmail.com>
2021-12-17 21:44:05 +0100

    UseConfiguration

--- net60/module3/before/Extensibility/PeopleViewer/Controllers/PeopleController.cs
+++ net60/module3/before/Extensibility/PeopleViewer/Controllers/PeopleController.cs
@@ -7,6 +7,15 @@ namespace PeopleViewer.Controllers;
 public class PeopleController : Controller
 {
     private ReaderFactory factory = new();
+    IConfiguration Configuration;
+    public PeopleController(IConfiguration configuration) => this.Configuration = configuration;
+
+    public IActionResult UseConfiguration()
+    {
+        string readerType = Configuration["PersonReaderType"];
+        ViewData["Title"] = "Using Configured Reader";
+        return PopulatePeopleView(readerType);
+    }
     public IActionResult UseService()
     {
         ViewData["Title"] = "Using a Web Service";
