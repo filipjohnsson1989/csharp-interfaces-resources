bb02a23f064be8a77225852a995a39aefed888f1
Filip Johnsson <Filip.johnsson1989@gmail.com>
2021-12-20 21:30:29 +0100

    Interfaces inheritance

---
+++ net60/module6/before/DefaultImplementation/Common/IPersonRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Common;
+
+public interface IPersonRepository : IPersonReader
+{
+    void AddPerson(Person newPerson);
+    void UpdatePerson(int id, Person updated);
+    void DeletePerson(int id);
+}
