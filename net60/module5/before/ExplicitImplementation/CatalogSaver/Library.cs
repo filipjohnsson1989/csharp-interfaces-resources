804061b3d415bc1db564af7219d5d7ba5f96f9ec
Filip Johnsson <Filip.johnsson1989@gmail.com>
2021-12-20 11:50:28 +0100

    Explicit Interface Implementation for Catalog class from ISaveable interface

--- net60/module5/before/ExplicitImplementation/CatalogSaver/Library.cs
+++ net60/module5/before/ExplicitImplementation/CatalogSaver/Library.cs
@@ -5,7 +5,10 @@ public interface ISaveable
     void Save();
 }
 
-public class Catalog
+public class Catalog : ISaveable
 {
-
+    void ISaveable.Save()
+    {
+        Console.WriteLine("Saved");
+    }
 }
--- net60/module5/before/ExplicitImplementation/CatalogSaver/Program.cs
+++ net60/module5/before/ExplicitImplementation/CatalogSaver/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        Catalog catalog = new();
+        //catalog.Save();
+
+        ISaveable saveable = new Catalog();
+        saveable.Save();
+
+        (catalog as ISaveable).Save();
+
+        var implicitCatalog = new Catalog();
+        //implicitCatalog.Save();
 
     }
 }
